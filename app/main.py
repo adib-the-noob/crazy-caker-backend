7d990b0398fc3b7619feea743db0cd98f41061b8
adib-the-noob <mdadib550@gmail.com>
2024-10-29 22:18:56 +0600

    cors added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from sqladmin import Admin, ModelView
 
 from contextlib import asynccontextmanager
@@ -26,6 +28,14 @@ app = FastAPI(
     version="0.1.0",
     lifespan=init_db
 )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # This allows requests from any IP address
+    allow_credentials=True,
+    allow_methods=["*"],   # This allows all HTTP methods
+    allow_headers=["*"],   # This allows all headers
+)
+
 admin = Admin(
     app=app,
     engine=engine, 
